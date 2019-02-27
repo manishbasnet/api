40bfa8e54e5fea5285efe70f1d49c1c084fd4c2a
GrowByData <manish.basnetnp@gmail.com>
2019-02-27 20:41:48 +0545

    basic skeleton for data profile generated

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    // Create json data from file
+    public function createJSONData()
+    {
+        // Read data from file
+
+        // Prepare JSON Format Data
+
+    }
+
+    public function getJSONData($profileId)
+    {
+        // Fetch data for profileId
+    }
+
+    // Get Profile
+    public function getProfile($profileId)
+    {
+        // Check if profileId exists in JSON Data
+
+        // Get data
+
+        $result = [
+            "category" => "cat1",
+            "risk_profile" => "risk",
+            "name" => "John",
+            "naics_code" => "abc",
+            "iso_gl_code" => "abc123",
+            "wc_codes" => "wc123",
+            "min_coverage" => "min cov",
+            "other_coverage" => "other cov"
+        ];
+        $profile = [
+            "id" => $profileId,
+            "result" => [$result]
+        ];
+
+        return json_encode($profile);
+    }
+
+}
